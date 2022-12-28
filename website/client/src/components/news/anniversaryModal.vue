0dbdd6427de4782d941f5b6eaf932da3dc1b0fb4
CuriousMagpie <eilatan@gmail.com>
2022-12-28 11:47:45 -0500

    starting anniversary modal

---
+++ website/client/src/components/news/anniversaryModal.vue
@@ -0,0 +1,40 @@
+<template>
+  <b-modal
+    id="anniversary-modal"
+    size="lg"
+    :hide-header="true"
+    :hide-footer="true"
+    no-close-on-esc
+    no-close-on-backdrop
+    @shown="onShow()"
+  >
+    <div class="modal-body">
+    </div>
+
+    <div class="modal-footer d-flex align-items-center pb-0">
+    </div>
+  </b-modal>
+</template>
+
+
+<style scoped lang="scss">
+  @import '~@/assets/scss/colors.scss';
+
+body {
+  padding: 32px 24px 24px;
+  background-image: linear-gradient(158deg, $purple-300, 0%, $purple-200, 100%);
+}
+
+</style>
+
+
+<script>
+
+
+export default {
+  components: {
+  },
+  methods: {
+  },
+};
+</script>
