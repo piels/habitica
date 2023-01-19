00771bea27ee888cdac367bf4ba5231dcffcd85e
SabreCat <sabe@habitica.com>
2023-01-19 11:42:35 -0600

    WIP(event): BBB

--- migrations/archive/2023/20230127_habit_birthday_day5.js
+++ migrations/archive/2023/20230127_habit_birthday_day5.js
@@ -23,7 +23,7 @@ async function updateUser (user) {
     data: {
       icon: 'notif_head_special_nye',
       title: 'Birthday Bash Day 5!',
-      text: 'Come celebrate by wearing your new Birthday Set!',
+      text: 'Come celebrate by wearing your new Habitica Hero Cape, Collar, and Mask!',
       destination: 'equipment',
     },
     seen: false,
